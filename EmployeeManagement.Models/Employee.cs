bc0f0142334fe87599be0dcafb52a0e9125da451
nuthan <inbox.nuthan@gmail.com>
2023-07-01 01:43:47 +0530

    Add project files.

---
+++ EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public Department Department { get; set; }
+
+        public string PhotoPath { get; set; } = string.Empty;
+
+    }
+}
